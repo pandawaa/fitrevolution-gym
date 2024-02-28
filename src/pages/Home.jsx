38eaf862e4bd61a9bae9ebe9082be201d5dccfe4
pandawaa <pallakangoding@gmail.com>
2024-02-28 16:37:28 +0800

    modified Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
+import { Box } from "@mui/material"
+
+import HeroBanner from "../components/HeroBanner"
+import SearchExercises from "../components/SearchExercises"
 
 const Home = () => {
-  return <div>Home</div>
+  const [exercises, setExercises] = useState([])
+  const [bodyPart, setBodyPart] = useState("all")
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExercise={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+    </Box>
+  )
 }
 
 export default Home
