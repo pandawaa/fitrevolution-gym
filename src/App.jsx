77630f79dcfccd5abc9cda4aca8294302251605b
pandawaa <pallakangoding@gmail.com>
2024-02-28 16:35:57 +0800

    modified App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ const App = () => (
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
+      {/* <Route path="/exercise/:id" element={<ExerciseDetail/>} /> */}
     </Routes>
     <Footer />
   </Box>
