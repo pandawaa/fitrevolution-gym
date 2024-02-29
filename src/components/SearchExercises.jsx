070452134573cc5f6d6ce67464b455292a17dc30
pandawaa <pallakangoding@gmail.com>
2024-02-29 12:42:27 +0800

    modified Search Exercises

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,27 +1,70 @@
+import React, { useEffect, useState } from "react"
 import { Box, Button, Stack, TextField, Typography } from "@mui/material"
-import React from "react"
 
-const SearchExercises = () => {
+import { exerciseOptions, fetchData } from "../utils/fetchData"
+import HorizontalScrollbar from "./HorizontalScrollbar"
+
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
+  const [search, setSearch] = useState("")
+  const [bodyParts, setBodyParts] = useState([])
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exerciseOptions
+      )
+
+      setBodyParts(["all", ...bodyPartsData])
+    }
+
+    fetchExercisesData()
+  }, [])
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        exerciseOptions
+      )
+
+      const searchedExercises = exercisesData.filter(
+        (item) =>
+          item.name.toLowerCase().includes(search) ||
+          item.target.toLowerCase().includes(search) ||
+          item.equipment.toLowerCase().includes(search) ||
+          item.bodyPart.toLowerCase().includes(search)
+      )
+
+      window.scrollTo({ top: 1800, left: 100, behavior: "smooth" })
+
+      setSearch("")
+      setExercises(searchedExercises)
+    }
+  }
+
   return (
-    <Stack alignItems="center" mt="37px" justifyContent="center" px="20px">
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
       <Typography
         fontWeight={700}
         sx={{ fontSize: { lg: "44px", xs: "30px" } }}
         mb="49px"
         textAlign="center"
       >
-        Awesome Exercise You <br /> Should Know
+        Awesome Exercises You <br /> Should Know
       </Typography>
       <Box position="relative" mb="72px">
         <TextField
           height="76px"
           sx={{
             input: { fontWeight: "700", border: "none", borderRadius: "4px" },
-            width: { lg: "800px", xs: "350px" },
+            width: { lg: "1170px", xs: "350px" },
             backgroundColor: "#fff",
             borderRadius: "40px",
           }}
-          placeholder="Search Exercise"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search Exercises"
           type="text"
         />
         <Button
@@ -36,9 +79,18 @@ const SearchExercises = () => {
             right: "0px",
             fontSize: { lg: "20px", xs: "14px" },
           }}
+          onClick={handleSearch}
         >
           Search
         </Button>
+      </Box>
+      <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyParts
+          setBodyPart={setBodyPart}
+          bodyPart={bodyPart}
+        />
       </Box>
     </Stack>
   )
